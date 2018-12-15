25a69ca32b774092fe13b3b0e6499560e21f33a8
Heikki Kupiainen <heikki.kupiainen@metamatic.net>
2018-12-15 17:05:02 +0200

    added functions to retrieve a nested object from inside a parent object

--- src/meta-object.js
+++ src/meta-object.js
@@ -1,11 +1,24 @@
 /*
 
-Testing out practical usage of dependencies in reusable libraries.
+A library for general purpose Metamatic object conversions
 
 Author: Heikki Kupiainen / Metamatic.net
 License: Apache 2.0
 */
 
-import {flattenObject} from '@metamatic.net/flatten-object';
+const getContainerData = (container, pathArray) => {
+  if (pathArray.length === 0) {
+    return container;
+  }
+  let nextProp = pathArray.shift();
+  const innerContainer = container[nextProp];
+  return innerContainer ? getContainerData(innerContainer, pathArray) : null;
+};
 
-export const delegateFlattenObject = (object) => flattenObject(object);
+export const getNestedObject = (object, nestedObjectPath) => {
+  if (!object) {
+    return null;
+  }
+  const pathArray = nestedObjectPath.split('.');
+  return getContainerData(object, pathArray);
+};

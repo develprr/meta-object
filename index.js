72e9c90710dc7fe0f210d6a7450940fb62fb256a
Heikki Kupiainen <heikki.kupiainen@metamatic.net>
2018-12-17 18:40:48 +0200

    new build

--- index.js
+++ index.js
@@ -13,11 +13,49 @@ License: Apache 2.0
 Object.defineProperty(exports, '__esModule', {
   value: true
 });
+var isObject = function isObject(item) {
+  return item && typeof item === 'object';
+};
 
-var _metamaticNetFlattenObject = require('@metamatic.net/flatten-object');
+var isArray = function isArray(item) {
+  return item && item instanceof Array;
+};
+
+var isContainer = function isContainer(item) {
+  return item && (typeof item === 'object' || item instanceof Array);
+};
+
+var arrayToPathmap = function arrayToPathmap(array, evolvingPath, pathmap) {
+  return array.forEach(function (arrayItem, index) {
+    var newPathGeneration = evolvingPath + '[' + index + ']';
+    objectToPathmap(arrayItem, newPathGeneration, pathmap);
+  });
+};
+
+var objectToPathmap = function objectToPathmap(object, evolvingPath, pathmap) {
+  pathmap = pathmap || {};
+  evolvingPath = evolvingPath || '';
+  var keys = Object.keys(object);
+  keys.forEach(function (key) {
+    var value = object[key];
+    var separator = evolvingPath.length > 0 ? '.' : '';
+    var newPathGeneration = evolvingPath + separator + key;
+    if (!isContainer(value)) {
+      pathmap[newPathGeneration] = value;
+      return pathmap;
+    }
+    if (isObject(value)) {
+      return objectToPathmap(value, newPathGeneration, pathmap);
+    }
+    if (isArray(value)) {
+      return arrayToPathmap(value, newPathGeneration, pathmap);
+    }
+  });
+  return pathmap;
+};
 
 var flattenObject = function flattenObject(object) {
-  return (0, _metamaticNetFlattenObject.makeObjectToFlat)(object, '', {});
+  return objectToPathmap(object, '', {});
 };
 exports.flattenObject = flattenObject;
 //# sourceMappingURL=flattenObject.js.map

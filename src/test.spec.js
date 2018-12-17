c1c2ed9d14e1106f2df23eecd035fc020bde2f6a
Heikki Kupiainen <heikki.kupiainen@metamatic.net>
2018-12-17 12:28:57 +0200

    created test for getting nested object

--- src/test.spec.js
+++ src/test.spec.js
@@ -1,5 +1,7 @@
 import {assert, describe, it} from 'mocha';
 import {delegateFlattenObject} from './meta-object';
+import {getNestedObject} from './getNestedObject';
+import {flattenObject} from './flattenObject';
 
 describe('flattenObject', () => {
 
@@ -13,9 +15,26 @@ describe('flattenObject', () => {
         }
       }
     }
-    const flatObject = delegateFlattenObject(deepObject);
+    const flatObject = flattenObject(deepObject);
     flatObject['user.kids.1'].should.equal('kim');
     flatObject['user.kids.2'].should.equal('jim');
     flatObject['user.address.streetAddress'].should.equal('Somestreet 1');
   });
 });
+
+describe('getNestedObject', () => {
+  it('should retrieve a nested object from inside another object', () => {
+    const deepObject = {
+      user: {
+        username: 'jondoe',
+        kids: ['tim', 'kim', 'jim'],
+        address: {
+          streetAddress: 'Somestreet 1'
+        }
+      }
+    }
+    const nestedObject = getNestedObject(deepObject, 'user.address');
+    nestedObject.streetAddress.should.equal('Somestreet 1');
+  });
+});
+

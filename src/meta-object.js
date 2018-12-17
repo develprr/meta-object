d112a5515609a67a03fa2a9e5c845cfb4069b8e8
Heikki Kupiainen <heikki.kupiainen@metamatic.net>
2018-12-17 13:54:11 +0200

    removed unused file

--- src/meta-object.js
+++
@@ -1,9 +0,0 @@
-/*
-A library for general purpose Metamatic object conversions
-
-Author: Heikki Kupiainen / Metamatic.net
-License: Apache 2.0
-*/
-
-import {getNestedObject} from './getNestedObject';
-import {flattenObject} from './flattenObject';

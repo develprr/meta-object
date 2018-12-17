2cf0372217906eb9026f9a1af4a1eb3e30bfbeb3
Heikki Kupiainen <heikki.kupiainen@metamatic.net>
2018-12-17 18:20:53 +0200

    removed dependency on flatten-object package

--- src/flattenObject.js
+++ src/flattenObject.js
@@ -1,3 +1,34 @@
-import {makeObjectToFlat} from '@metamatic.net/flatten-object';
+const isObject = (item) => item && (typeof item === 'object');
 
-export const flattenObject = (object) => makeObjectToFlat(object, '', {});
+const isArray = (item) => item && item instanceof Array;
+
+const isContainer = (item) => item && (typeof item === 'object' || item instanceof Array);
+
+const arrayToPathmap = (array, evolvingPath, pathmap) => array.forEach((arrayItem, index) => {
+  const newPathGeneration = evolvingPath + '[' + index + ']';
+  objectToPathmap(arrayItem, newPathGeneration, pathmap);
+});
+
+const objectToPathmap = (object, evolvingPath, pathmap) => {
+  pathmap = pathmap || {};
+  evolvingPath = evolvingPath || '';
+  const keys = Object.keys(object);
+  keys.forEach(key => {
+    const value = object[key];
+    const separator = evolvingPath.length > 0 ? '.' : '';
+    const newPathGeneration = evolvingPath + separator + key;
+    if (!isContainer(value)) {
+      pathmap[newPathGeneration] = value;
+      return pathmap
+    }
+    if (isObject(value)) {
+      return objectToPathmap(value, newPathGeneration, pathmap);
+    }
+    if (isArray(value)) {
+      return arrayToPathmap(value, newPathGeneration, pathmap);
+    }
+  })
+  return pathmap;
+}
+
+export const flattenObject = (object) => objectToPathmap(object, '', {});

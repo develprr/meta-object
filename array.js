88576fe1d80000555b908619b8913d001e1d96c9
Heikki Kupiainen <heikki.kupiainen@metamatic.net>
2019-01-02 14:53:54 +0200

    fixed issue: package was not built

--- array.js
+++ array.js
@@ -3,11 +3,11 @@
 Object.defineProperty(exports, '__esModule', {
   value: true
 });
-var sortAlphabetically = function sortAlphabetically(array, column) {
+var sortAlphabeticallyByProperty = function sortAlphabeticallyByProperty(array, property, sortDirection) {
   return array.sort(function (a, b) {
-    var sortDirectionSwapper = column.sortDirection === 'desc' ? -1 : 1;
-    var nameA = a[column.id].toLowerCase();
-    var nameB = b[column.id].toLowerCase();
+    var sortDirectionSwapper = sortDirection === 'desc' ? -1 : 1;
+    var nameA = a[property].toLowerCase();
+    var nameB = b[property].toLowerCase();
     if (nameA < nameB) {
       return -1 * sortDirectionSwapper;
     }
@@ -17,5 +17,5 @@ var sortAlphabetically = function sortAlphabetically(array, column) {
     return 0;
   });
 };
-exports.sortAlphabetically = sortAlphabetically;
+exports.sortAlphabeticallyByProperty = sortAlphabeticallyByProperty;
 //# sourceMappingURL=sortAlphabeticallyByProperty.js.map
